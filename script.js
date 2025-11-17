10a7f506f3c7cd12a3ce1ecd0087ad02f613d507
Johnpii1 <okolichimobi08gmail.com>
2025-11-17 11:59:51 +0100

    update slide

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const img = document.getElementById('imgMove');
 
-img.addEventListener('click', () => {
-  img.classList.toggle('translate-x-8'); // moves 2rem on click
-});
+  const slider = document.getElementById("slider");
+  const nextBtn = document.getElementById("nextBtn");
+  const backBtn = document.getElementById("backBtn");
+
+  nextBtn.addEventListener("click", () => {
+    slider.scrollBy({ left: 260, behavior: "smooth" });
+  });
+
+  backBtn.addEventListener("click", () => {
+    slider.scrollBy({ left: -260, behavior: "smooth" });
+  });
+
