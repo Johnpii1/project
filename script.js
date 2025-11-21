5f620da5b86c738bf62a01103e5b6e5ea672a12c
Johnpii1 <okolichimobi08gmail.com>
2025-11-21 09:38:34 +0100

    update all slide

--- script.js
+++ script.js
@@ -11,3 +11,29 @@
     slider.scrollBy({ left: -260, behavior: "smooth" });
   });
 
+
+  //2
+  const slider2 = document.getElementById("slider2");
+  const nextBtn2 = document.getElementById("nextBtn2");
+  const backBtn2 = document.getElementById("backBtn2");
+
+  nextBtn2.addEventListener("click", () => {
+    slider2.scrollBy({left: 260, behavior: "smooth"});
+  });
+backBtn2.addEventListener("click", () => {
+    slider2.scrollBy({left: -260, behavior: "smooth"});
+  });
+
+  //FOR SLIDE4 
+  const slider4 = document.getElementById("slider4");
+  const nextBtn4 = document.getElementById("nextBtn4");
+  const backBtn4 = document.getElementById("backBtn4");
+
+  nextBtn4.addEventListener("click", () => {
+    slider4.scrollBy({ left: 260, behavior: "smooth" });
+  });
+
+  backBtn4.addEventListener("click", () => {
+    slider4.scrollBy({ left: -260, behavior: "smooth" });
+  });
+
