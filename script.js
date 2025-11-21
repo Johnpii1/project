981c0220dd6abe58d6afdf0da9c8e19c4cec86de
Johnpii1 <okolichimobi08gmail.com>
2025-11-21 12:33:55 +0100

    all phone store

--- script.js
+++ script.js
@@ -40,12 +40,76 @@ backBtn3.addEventListener("click", () => {
 //FOR SLIDE4
 const slider4 = document.getElementById("slider4");
 const nextBtn4 = document.getElementById("nextBtn4");
-const backBtn4 = document.getElementById("backBtn4");
+const backBtn4 = document.getElementById("backBtn4")
 
 nextBtn4.addEventListener("click", () => {
-  slider4.scrollBy({ left: 260, behavior: "smooth" });
+  slider4.scrollBy({left: 260, behavior: "smooth"})
+});
+backBtn4.addEventListener("click", () => {
+  slider4.scrollBy({left: -260, behavior: "smooth"})
 });
 
-backBtn4.addEventListener("click", () => {
-  slider4.scrollBy({ left: -260, behavior: "smooth" });
+//FOR SLIDE5
+const slider5 = document.getElementById("slider5");
+const nextBtn5 = document.getElementById("nextBtn5");
+const backBtn5 = document.getElementById("backBtn5");
+
+nextBtn5.addEventListener("click", () => {
+  slider5.scrollBy({ left: 260, behavior: "smooth" });
+});
+
+backBtn5.addEventListener("click", () => {
+  slider5.scrollBy({ left: -260, behavior: "smooth" });
+});
+
+//FOR SLIDE6
+const slider6 = document.getElementById("slider6");
+const nextBtn6 = document.getElementById("nextBtn6");
+const backBtn6 = document.getElementById("backBtn6");
+
+nextBtn6.addEventListener("click", () => {
+  slider6.scrollBy({ left: 260, behavior: "smooth" });
+});
+
+backBtn6.addEventListener("click", () => {
+  slider6.scrollBy({ left: -260, behavior: "smooth" });
+});
+
+//FOR SLIDE7
+const slider7 = document.getElementById("slider7");
+const nextBtn7 = document.getElementById("nextBtn7");
+const backBtn7 = document.getElementById("backBtn7");
+
+nextBtn7.addEventListener("click", () => {
+  slider7.scrollBy({ left: 260, behavior: "smooth" });
+});
+
+backBtn7.addEventListener("click", () => {
+  slider7.scrollBy({ left: -260, behavior: "smooth" });
+});
+
+//FOR SLIDE8
+const slider8 = document.getElementById("slider6");
+const nextBtn8 = document.getElementById("nextBtn8");
+const backBtn8 = document.getElementById("backBtn8");
+
+nextBtn8.addEventListener("click", () => {
+  slider8.scrollBy({ left: 260, behavior: "smooth" });
+});
+
+backBtn8.addEventListener("click", () => {
+  slider8.scrollBy({ left: -260, behavior: "smooth" });
+});
+
+//FOR SLIDE9
+const slider9 = document.getElementById("slider9");
+const nextBtn9 = document.getElementById("nextBtn9");
+const backBtn9 = document.getElementById("backBtn9");
+
+nextBtn6.addEventListener("click", () => {
+  slider9.scrollBy({ left: 260, behavior: "smooth" });
+});
+
+backBtn9.addEventListener("click", () => {
+  slider9.scrollBy({ left: -260, behavior: "smooth" });
 });
